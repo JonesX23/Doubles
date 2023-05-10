974f97288f9961c75f55fd545aa0bcf3a62f9b68
JonesX23 <jonasklose@hotmail.de>
2023-05-10 10:35:13 +0200

    fuzzy similarity check

---
+++ fuzz.py
@@ -0,0 +1,57 @@
+from fuzzywuzzy import fuzz
+import pandas as pd
+
+# Load the data into a DataFrame
+
+df = pd.read_csv("mitglieder.csv", on_bad_lines='skip', encoding="ISO-8859-1", sep=';', engine='python')
+# Remove exact duplicates
+# Load the data into a DataFrame
+
+# Remove exact duplicates
+df.drop_duplicates(inplace=True)
+
+
+# Define a function to compare two strings and return a similarity score
+def similarity(s1, s2):
+    return fuzz.token_sort_ratio(str(s1), str(s2))
+
+
+# Define a threshold similarity score
+threshold = 90
+
+# Create an empty DataFrame to store the unique entries
+unique = pd.DataFrame(columns=df.columns)
+
+# Create a new DataFrame to store the non-unique entries
+non_unique = pd.DataFrame(columns=df.columns)
+
+# Loop through each row of the DataFrame
+for i, row1 in df.iterrows():
+
+    # Assume the current row is unique
+    is_unique = True
+
+    # Compare the current row to all previous rows
+    for j, row2 in unique.iterrows():
+
+        # Compare the fields (Name, Adresse, Ansprechperson, Email) using the rapidfuzz library
+        sim1 = similarity(row1['Name'], row2['Name'])
+        sim2 = similarity(row1['Adresse'], row2['Adresse'])
+        sim3 = similarity(row1['Ansprechperson'], row2['Ansprechperson'])
+        sim4 = similarity(row1['Email 1'], row2['Email 1'])
+
+        # If the similarity score is above the threshold, the rows are considered duplicates
+        if sim1 >= threshold and sim2 >= threshold and sim3 >= threshold and sim4 >= threshold:
+            is_unique = False
+            break
+
+    # If the row is unique, add it to the unique DataFrame; otherwise, add it to the non_unique DataFrame
+    if is_unique:
+        unique = unique._append(row1, ignore_index=True)
+    else:
+        non_unique = non_unique._append(row1, ignore_index=True)
+
+# The unique DataFrame contains only the unique entries
+unique.to_csv('UNIQUE3.csv')
+unique.to_excel('UNIQUE-EXCEL3.xlsx')
+non_unique.to_excel('removed.xlsx')
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import csv
 import pandas as pd
+import numpy as np
+from fuzzywuzzy import fuzz
 
 data = pd.read_csv("mitglieder.csv", on_bad_lines='skip', encoding="ISO-8859-1", sep=';', engine='python')
 data.dropna(subset=['Name'], how='any', inplace=True)
@@ -15,24 +17,66 @@ data.dropna(subset=data.columns.difference(['Name']), how='all', inplace=True)
 
 duplicates = data[data.duplicated()]
 # Loop through each duplicate row and print the selected columns
-for index, row in duplicates.iterrows():
-    print(f"Duplicate {index + 1}:")
-    print(row.loc[['Name']])
+# for index, row in duplicates.iterrows():
+#     print(f"Duplicate {index + 1}:")
+#     print(row.loc[['Name']])
 
-data2 = data.drop_duplicates()
+data = data.drop_duplicates()
+#
+# similarity_threshold = 0.1 # Set the similarity threshold here
+# similarity_cols = ['Name']  # Set the columns to use for similarity comparison here
+# similarity_group = data2.groupby(similarity_cols).size().reset_index(name='count')
+# possible_duplicates = similarity_group[similarity_group['count'] > 1]
 
-similarity_threshold = 0.5 # Set the similarity threshold here
-similarity_cols = ['Name', 'Adresse ', 'Ort ',
-                   'Ansprechperson ']  # Set the columns to use for similarity comparison here
-similarity_group = data2.groupby(similarity_cols).size().reset_index(name='count')
-possible_duplicates = similarity_group[similarity_group['count'] > 1]
 
-# Print the possible duplicates
-print("ALLE DUPLIKATE")
-print(data.duplicated)
-print("MÖGLICHE DUPLIKATE NACH ÄHNLICHKEIT")
-print(possible_duplicates)
+# similarity_threshold = 0.1
+# similarity_cols = ['Name', 'PLZ', 'Telefon 1']
+# similarity_group = data.groupby(similarity_cols).size().reset_index(name='count')
+# possible_duplicates = similarity_group[similarity_group['count'] > 1]
+#
+# # Create a new DataFrame to store the rows with the most complete information
+# filtered_data = pd.DataFrame(columns=data.columns)
+#
+# # Iterate through each group of possible duplicates
+# for _, group in possible_duplicates.groupby(similarity_cols):
+#     # Create a list to store the completeness score for each row in the group
+#     completeness_scores = []
+#     for index, row in group.iterrows():
+#         # Calculate the completeness score for the row
+#         completeness_score = 1 - np.sum(row.isnull()) / len(row)
+#         completeness_scores.append(completeness_score)
+#     # Find the index of the row with the highest completeness score
+#     max_index = completeness_scores.index(max(completeness_scores))
+#     # Add the row with the highest completeness score to the new DataFrame
+#     filtered_data = filtered_data._append(group.iloc[max_index], ignore_index=True)
+
+# define a similarity threshold (e.g., 80%)
+threshold = 80
+
+# compare each row to the others and remove duplicates based on similarity score
+to_remove = set()
+for i, row1 in data.iterrows():
+    for j, row2 in data.iterrows():
+        if i == j:
+            continue
+        similarity = fuzz.token_set_ratio(row1, row2)
+        if similarity >= threshold:
+            to_remove.add(j)
+print("REMOVE BY FUZZYWUZZY")
+print(to_remove)
+#df = df.drop(to_remove)
+
+
+
+# Print the new DataFrame
+#print("FILTERED DATA")
+#print(filtered_data)
+
+##Print the possible duplicates
+# print("ALLE DUPLIKATE")
+# print(data.duplicated)
+#print("MÖGLICHE DUPLIKATE NACH ÄHNLICHKEIT")
+#print(possible_duplicates)
 # data2.to_csv('Mitglieder-bereinigt.csv')
 # data2.to_excel('Mitglieder.bereinigt.xlsx')
-
-print(data2)
+# filtered_data.to_excel('DuplettenMultipleAnsprechpartner.xlsx')
